855714cc04b3047ad1c0876c97f0f4c6c6def765
Taylor Stevens <taylorstevens14562@gmail.com>
2024-05-18 15:48:22 -0400

    bio update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,30 +12,30 @@
                     </v-card-item>
                     <v-card-text>
                         <br/>
-                        Silvia Gosálvez is currently in her fifth year of her Dual Degree in Law and International Relations at 
-                        IE University in Madrid, a univeristy in her hometown. Most recently, she spent a semester abroad studying
-                        at Northeastern University in Boston. During that time she held a position as an art law intern at the 
-                        Volunteer Lawyers for the Arts department in the Arts and Business Council of Greater Boston. Here she
-                        provided pro bono legal advice to artists and creators.
+                        Silvia Gosálvez is currently in her fifth year of her Dual Degree in Law and International Relations 
+                        at IE University in Madrid, her hometown. Most recently, she spent a semester abroad studying at 
+                        Northeastern University in Boston. During that time she held a position as an art law intern at 
+                        the Volunteer Lawyers for the Arts department in the Arts and Business Council of Greater Boston. 
+                        Here she provided pro bono legal advice to artists and creators.
                         <br/><br/>
-                        Alongside this, she has collaborated with two NGOs, The Meraki Smile School in Madagascar and Fundación 
-                        Educación para el Empleo. As part of her legal training, she has held two internship contracts, the first 
-                        in the IP/IT department in ECIJA and the second in the multidisciplinary law firm Delyser Abogados.
+                        Alongside this, she has collaborated with two NGOs, Meraki Smile School in Madagascar and Fundación 
+                        Educación para el Empleo. As part of her legal training, she has held two internship contracts, in 
+                        the IP/IT department at ECIJA and in the multidisciplinary law firm Delyser Abogados.
                         <br/><br/>
-                        Her passion for art, heritage conservation, and multiculturalism has led her to orient her professional 
-                        development to the legal management of arts and cultural heritage. 
+                        Her passion for art, heritage conservation, and multiculturalism has led her to orient her 
+                        professional development to the legal management of arts and cultural heritage.
                         <br/><br/>
-                        Spanish being her mother tongue, she is also fluent in English and proficient in French. Further she is 
-                        conversational in both Portuguese and Arabic. 
+                        Spanish being her mother tongue, she is also fluent in English and proficient in French. Further 
+                        she is conversational in both Portuguese and Arabic.
                         <br/><br/>
-                        Involved in her university's campus life, she has been President of the IE Photography Club and is an active 
-                        member of the Innovation and Inclusion committee at IE Law School. Here she was able to successfully organize
-                        the event "Breaking Barriers" on LGBTQ+ inclusion in the education and labour sector. Academically, she is a 
-                        member of the "Honors Program", a program composed of the top 5% of students with the best academic records 
-                        at the university. She has also participated in IE's Legal Clinic, this last year acting as a project 
-                        coordinator and teaching assistant. 
+                        Involved in her university's campus life, she has been President of the IE Photography Club and 
+                        is a board member of the Innovation and Inclusion committee at IE Law School. Here she was able to 
+                        successfully organize the event "Breaking Barriers" on LGBTQ+ inclusion in the education and labour 
+                        sector. Academically, she is a member of the "Honors Program", a program composed of the top 5% of 
+                        students with the best academic records at the university. She has also participated in IE's Legal 
+                        Clinic, this last year acting as a project coordinator and teaching assistant.
                         <br/><br/>
-                        An interesting fact about her is that her interpersonal communication skills allowed her to spend a 
+                        An interesting fact about her is that her interpersonal communication skills allowed her to spend a
                         summer working as a tour guide in Porto, Portugal, a city she had never been to before.
                     </v-card-text>
                 </v-card>
